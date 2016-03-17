09bae7f3bf77295d00970038e090c1f1c961f98d
Robert Duriancik <r.duriancik@gmail.com>
2016-03-17 22:59:48 +0100

    Vytvorene metody v Customer

--- src/main/java/cz/muni/fi/pv168/book_rental/Customer.java
+++ src/main/java/cz/muni/fi/pv168/book_rental/Customer.java
@@ -1,7 +1,9 @@
 package cz.muni.fi.pv168.book_rental;
 
+import java.util.regex.Pattern;
+
 /**
- * Created by Milan on 26.02.2016.
+ * Created by Robert Duriancik on 26.02.2016.
  */
 public class Customer {
     private Long id;
@@ -16,8 +18,13 @@ public class Customer {
         return phoneNumber;
     }
 
+    // format "+xxxxxxxxxxxx"
     public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        if (isValidPhoneNumber(phoneNumber)) {
+            this.phoneNumber = phoneNumber;
+        } else {
+            throw new IllegalArgumentException("Invalid phone number");
+        }
     }
 
     public Long getId() {
@@ -33,15 +40,49 @@ public class Customer {
     }
 
     public void setName(String name) {
-        this.name = name;
+        if (isValidName(name)) {
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Invalid name");
+        }
     }
 
     public String getAddress() {
         return address;
     }
 
+    // pattern "street streetNumber, zipCode(xxx xx) city"
     public void setAddress(String address) {
-        this.address = address;
+        if (isValidAddress(address)) {
+            this.address = address;
+        } else {
+            throw new IllegalArgumentException("Invalid address");
+        }
+    }
+
+    private boolean isValidName(String name) {
+        if (name == null || name.isEmpty()) {
+            return false;
+        }
+
+        return Pattern.matches("[a-zA-Z\\u00c0-\\u017e]", name);
+    }
+
+    private boolean isValidPhoneNumber(String number) {
+        if (number == null || number.isEmpty()) {
+            return false;
+        }
+
+        return Pattern.matches("[+]\\d{12}+", number);
+    }
+
+    private boolean isValidAddress(String address) {
+        if (address == null || address.isEmpty()) {
+            return false;
+        }
+
+        return Pattern.matches("[a-zA-Z0-9\\u00c0-\\u017e -]+[,][ ]" +
+                "[0-9]{3}+[ ][0-9]{2}+[ ][a-zA-Z0-9\\u00c0-\\u017e., -]+", address);
     }
 
 }
